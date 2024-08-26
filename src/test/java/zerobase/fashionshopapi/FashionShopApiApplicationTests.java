af4c3d84db8a3c99860aadda926a3cdc23ef83cb
jiwookwon0812 <kwonjiwoo@naver.com>
2024-08-26 15:04:53 +0900

    commit success

---
+++ src/test/java/zerobase/fashionshopapi/FashionShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package zerobase.fashionshopapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashionShopApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
