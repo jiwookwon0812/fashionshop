d6c10a15a1a3d00a09652de5301bac38711f6276
jiwookwon0812 <kwonjiwoo@naver.com>
2024-08-26 14:14:17 +0900

    commit success

---
+++ src/main/java/zerobase/fashonshopapi/FashonShopApiApplication.java
@@ -0,0 +1,13 @@
+package zerobase.fashonshopapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashonShopApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FashonShopApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/zerobase/fashonshopapi/FashonShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package zerobase.fashonshopapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashonShopApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
